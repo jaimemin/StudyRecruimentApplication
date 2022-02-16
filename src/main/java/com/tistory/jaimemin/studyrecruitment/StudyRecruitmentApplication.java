4ade620193fded71ae56fec8dd9a18882db50dc3
jaimemin <jaimemin@gmail.com>
2022-02-16 23:29:50 +0900

    Initial commit

---
+++ src/main/java/com/tistory/jaimemin/studyrecruitment/StudyRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.jaimemin.studyrecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRecruitmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRecruitmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tistory/jaimemin/studyrecruitment/StudyRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.jaimemin.studyrecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRecruitmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
