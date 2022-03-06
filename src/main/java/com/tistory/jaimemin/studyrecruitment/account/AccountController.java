b2153f85527d476765cb5b3f960acb93b7238668
jaimemin <jaimemin@gmail.com>
2022-03-06 21:51:28 +0900

    implemented sign-up form validation

--- src/main/java/com/tistory/jaimemin/studyrecruitment/account/AccountController.java
+++ src/main/java/com/tistory/jaimemin/studyrecruitment/account/AccountController.java
@@ -1,8 +1,15 @@
 package com.tistory.jaimemin.studyrecruitment.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 /**
  * @author jaime
@@ -12,12 +19,30 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @since 2022-03-06
  */
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String singUpForm(Model model) {
         model.addAttribute(new SignUpForm());
 
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        // TODO 회원 가입 처리
+        return "redirect:/";
     }
 }
--- src/main/java/com/tistory/jaimemin/studyrecruitment/account/SignUpForm.java
+++ src/main/java/com/tistory/jaimemin/studyrecruitment/account/SignUpForm.java
@@ -1,6 +1,11 @@
 package com.tistory.jaimemin.studyrecruitment.account;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 /**
  * @author jaime
@@ -12,9 +17,16 @@ import lombok.Data;
 @Data
 public class SignUpForm {
 
+    @NotBlank
+    @Length(min = 3, max = 20)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
     private String nickname;
 
+    @Email
+    @NotBlank
     private String email;
 
+    @NotBlank
+    @Length(min = 8, max = 50)
     private String password;
 }
