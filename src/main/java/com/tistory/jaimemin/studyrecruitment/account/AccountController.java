d7bca11f5552c34d563bf2b8045eca87215da287
jaimemin <jaimemin@gmail.com>
2022-03-06 19:19:02 +0900

    implemented sign-up controller

---
+++ src/main/java/com/tistory/jaimemin/studyrecruitment/account/AccountController.java
@@ -0,0 +1,21 @@
+package com.tistory.jaimemin.studyrecruitment.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author jaime
+ * @title AccountController
+ * @see\n <pre>
+ * </pre>
+ * @since 2022-03-06
+ */
+@Controller
+public class AccountController {
+
+    @GetMapping("/sign-up")
+    public String singUpForm(Model model) {
+        return "account/sign-up";
+    }
+}
---
+++ src/main/java/com/tistory/jaimemin/studyrecruitment/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.tistory.jaimemin.studyrecruitment.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author jaime
+ * @title SecurityConfig
+ * @see\n <pre>
+ * </pre>
+ * @since 2022-03-06
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token"
+                        , "/email-login", "/check-email-login", "/login-link")
+                .permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
