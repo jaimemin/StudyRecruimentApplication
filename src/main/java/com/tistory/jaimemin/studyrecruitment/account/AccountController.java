9e0c5175b7a4562136f03579385f9add942724ea
jaimemin <jaimemin@gmail.com>
2022-03-16 00:05:42 +0900

    refactored code

--- src/main/java/com/tistory/jaimemin/studyrecruitment/account/AccountController.java
+++ src/main/java/com/tistory/jaimemin/studyrecruitment/account/AccountController.java
@@ -1,9 +1,6 @@
 package com.tistory.jaimemin.studyrecruitment.account;
 
-import com.tistory.jaimemin.studyrecruitment.domain.Account;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -27,9 +24,7 @@ public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
 
-    private final AccountRepository accountRepository;
-
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
 
     @InitBinder("signUpForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -49,25 +44,7 @@ public class AccountController {
             return "account/sign-up";
         }
 
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODO encoding 해야함
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdatedByWeb(true)
-                .build();
-        Account newAccount = accountRepository.save(account);
-        newAccount.generateEmailCheckToken();
-
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setSubject("Study-Recruitment, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token="
-                + newAccount.getEmailCheckToken()
-                + "&email="
-                + newAccount.getEmail());
-        javaMailSender.send(mailMessage);
+        accountService.processNewAccount(signUpForm);
 
         // TODO 회원 가입 처리
         return "redirect:/";
--- src/test/java/com/tistory/jaimemin/studyrecruitment/account/AccountControllerTest.java
+++ src/test/java/com/tistory/jaimemin/studyrecruitment/account/AccountControllerTest.java
@@ -2,13 +2,20 @@ package com.tistory.jaimemin.studyrecruitment.account;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -18,6 +25,12 @@ class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @MockBean
+    JavaMailSender javaMailSender;
+
     @DisplayName("회원 가입 화면 보이는지 테스트")
     @Test
     void signUpForm() throws Exception {
@@ -25,5 +38,32 @@ class AccountControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("account/sign-up"))
                 .andExpect(model().attributeExists("signUpForm"));
+    }
+
+    @DisplayName("회원 가입 처리 - 입력값 오류")
+    @Test
+    void signUpSubmitWithWrongInput() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("nickname", "jaimemin")
+                .param("email", "email..")
+                .param("password", "12345")
+                .with(csrf())) // permitAll()이여도 csrf token이 필요함
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+
+    @DisplayName("회원 가입 처리 - 입력값 정상")
+    @Test
+    void signUpSubmitWithCorrectInput() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                        .param("nickname", "jaimemin")
+                        .param("email", "jaimemin@tistory.com")
+                        .param("password", "12345678")
+                        .with(csrf())) // permitAll()이여도 csrf token이 필요함
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        assertTrue(accountRepository.existsByEmail("jaimemin@tistory.com"));
+        then(javaMailSender).should().send(ArgumentMatchers.any(SimpleMailMessage.class));
     }
 }
