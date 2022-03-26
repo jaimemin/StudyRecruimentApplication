5c1ac2e178b645e660d770d6f36992822570d465
jaimemin <jaimemin@gmail.com>
2022-03-27 00:52:58 +0900

    implemented remember-me

---
+++ src/main/java/com/tistory/jaimemin/studyrecruitment/domain/PersistentLogins.java
@@ -0,0 +1,38 @@
+package com.tistory.jaimemin.studyrecruitment.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+/**
+ * @author jaime
+ * @title PersistentLogins
+ * @see\n <pre>
+ * </pre>
+ * @since 2022-03-27
+ */
+@Table(name = "persistent_logins")
+@Entity
+@Getter
+@Setter
+public class PersistentLogins {
+
+    @Id
+    @Column(length = 64)
+    private String series;
+
+    @Column(nullable = false, length = 64)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String token;
+
+    @Column(name = "last_used", nullable = false, length = 64)
+    private LocalDateTime lastUsed;
+
+}
