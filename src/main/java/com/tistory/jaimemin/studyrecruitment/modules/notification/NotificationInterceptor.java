60eb00156c40d47c9873caa2ae31d49f55eecde9
jaimemin <jaimemin@gmail.com>
2022-05-17 23:56:12 +0900

    implemented HandlerInterceptor for notification

--- src/main/java/com/tistory/jaimemin/studyrecruitment/modules/notification/NotificationInterceptor.java
+++ src/main/java/com/tistory/jaimemin/studyrecruitment/modules/notification/NotificationInterceptor.java
@@ -3,6 +3,7 @@ package com.tistory.jaimemin.studyrecruitment.modules.notification;
 import com.tistory.jaimemin.studyrecruitment.modules.account.Account;
 import com.tistory.jaimemin.studyrecruitment.modules.account.UserAccount;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -20,11 +21,12 @@ import javax.servlet.http.HttpServletResponse;
  * </pre>
  * @since 2022-05-17
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NotificationInterceptor implements HandlerInterceptor {
 
-    private NotificationRepository notificationRepository;
+    private final NotificationRepository notificationRepository;
 
     @Override
     public void postHandle(HttpServletRequest request
@@ -34,11 +36,12 @@ public class NotificationInterceptor implements HandlerInterceptor {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (modelAndView != null
-                && isRedirectView(modelAndView)
+                && !isRedirectView(modelAndView)
                 && authentication != null
                 && authentication.getPrincipal() instanceof UserAccount) {
             Account account = ((UserAccount)authentication.getPrincipal()).getAccount();
             long count = notificationRepository.countByAccountAndChecked(account, false);
+            log.info("hasNotification {}", count > 0);
 
             modelAndView.addObject("hasNotification", count > 0);
         }
